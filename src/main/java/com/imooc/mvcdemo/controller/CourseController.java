10b93473622c79d1fcc59281a52dc8aa855c809c
zjxjwxk <zjxjwxk@gmail.com>
2018-09-16 15:18:56 +0800

    Update

--- src/main/java/com/imooc/mvcdemo/controller/CourseController.java
+++ src/main/java/com/imooc/mvcdemo/controller/CourseController.java
@@ -2,15 +2,21 @@ package com.imooc.mvcdemo.controller;
 
 import com.imooc.mvcdemo.model.Course;
 import com.imooc.mvcdemo.service.CourseService;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @Controller
@@ -75,5 +81,32 @@ public class CourseController {
         course.setCourseId(123);
 
         return "redirect:view2/" + course.getCourseId();
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String showUploadPage() {
+        return "course_admin/file";
+    }
+
+    @RequestMapping(value = "/doUpload", method = RequestMethod.POST)
+    public String doUploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+
+        if (!file.isEmpty()) {
+            log.debug("Process file:{}", file.getOriginalFilename());
+            FileUtils.copyInputStreamToFile(file.getInputStream(), new File("D:\\temp\\imooc\\", System.currentTimeMillis() + file.getOriginalFilename()));
+        }
+
+        return "success";
+    }
+
+    @RequestMapping(value = "/{courseId}", method = RequestMethod.GET)
+    public @ResponseBody Course getCourseInJson(@PathVariable Integer courseId) {
+        return courseService.getCoursebyId(courseId);
+    }
+
+    @RequestMapping(value = "/jsontype/{courseId}", method = RequestMethod.GET)
+    public ResponseEntity<Course> getCourseInJson2(@PathVariable Integer courseId) {
+        Course course = courseService.getCoursebyId(courseId);
+        return new ResponseEntity<Course>(course, HttpStatus.OK);
     }
 }
